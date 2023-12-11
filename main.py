8ef70ed3e0fb32a9857ad8b6c54eaccebbc06cd1
aleksey <avdivo@yahoo.com>
2023-12-12 02:17:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def process():
+    return "Hello"
+    headers = dict(request.headers)
+    data = request.get_data()
+    cookies = dict(request.cookies)
+    get_data = dict(request.args)
+    print('-----', get_data)
+    if request.method == 'POST':
+        # Обработка POST
+        data = request.get_json()
+    else:
+        # Обработка GET
+        data = request.args
+
+    response = requests.post("http://example.com/api", json=data)
+    result = response.json()
+
+    print(f"Server response: {result}")
+
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run()
+
